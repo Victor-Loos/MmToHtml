c1d99ad29ba37679ce67209876a8a04ac9e3ed03
Victor-Loos <loosvictor0@gmail.com>
2022-01-18 09:44:40 +0100

    header

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementsByClassName("bouton")
+        .addEventListener("click", function() {
+  document.getElementById("bienvenue").hidden = true;
+  document.getElementById("impressionnant").hidden = false;
+}, false);
+
